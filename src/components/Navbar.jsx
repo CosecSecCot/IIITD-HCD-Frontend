74b25988ce96fb13f5052865c59d685e6b97268d
Jagjot Singh <cosecseccot@CosecSecCot-2.local>
2025-07-08 10:50:31 +0530

    animations: page-reveal and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,10 @@ import HCDLogoLarge from "../assets/HCD_logo_with_text_large.svg";
 import IIITDLogo from "../assets/IIITD_logo.svg";
 import { ChevronRight, Menu, Search, X } from "lucide-react";
 import { useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 const navigationMap = [
   {
@@ -48,7 +52,94 @@ const navigationMap = [
 
 const Navbar = () => {
   const [activeGroup, setActiveGroup] = useState();
-  const [sideBarOpen, setSideBarOpen] = useState(false);
+  const [firstSidebarOpen, setFirstSidebarOpen] = useState(false);
+  const [secondSidebarOpen, setSecondSidebarOpen] = useState(false);
+
+  const { contextSafe } = useGSAP(() => {});
+
+  const openFirstSidebar = contextSafe(() => {
+    setFirstSidebarOpen(true);
+    const timeline = gsap.timeline();
+    timeline
+      .fromTo(
+        "#nav-bg",
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          ease: "expo.out",
+          duration: "0.5",
+        },
+      )
+      .to(
+        "#nav-container",
+        {
+          x: "50%",
+          ease: "expo.out",
+        },
+        "-=0.5",
+      )
+      .fromTo(
+        "#nav-link-primary",
+        {
+          opacity: 0,
+          y: 30,
+          clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+        },
+        {
+          opacity: 1,
+          y: 0,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          duration: 0.5,
+          ease: "power3.out",
+          stagger: 0.1,
+        },
+        "-=0.5",
+      );
+  });
+
+  const closeSidebar = contextSafe(() => {
+    const timeline = gsap.timeline();
+    timeline
+      .to("#nav-container", {
+        x: "50%",
+        ease: "expo.in",
+        duration: secondSidebarOpen ? 0.2 : 0,
+      })
+      .to("#nav-container", {
+        x: "100%",
+        ease: "expo.out",
+      })
+      .fromTo(
+        "#nav-bg",
+        {
+          opacity: 1,
+        },
+        {
+          opacity: 0,
+          ease: "expo.out",
+          duration: "0.5",
+        },
+        "-=0.5",
+      )
+      .then(() => {
+        if (secondSidebarOpen) {
+          setSecondSidebarOpen(false);
+        }
+        setFirstSidebarOpen(false);
+        setActiveGroup(null);
+      });
+  });
+
+  const openSecondSidebar = contextSafe(() => {
+    setSecondSidebarOpen(true);
+    const timeline = gsap.timeline();
+    timeline.to("#nav-container", {
+      x: 0,
+      ease: "expo.out",
+    });
+  });
 
   return (
     <header className="absolute z-50 top-0 w-full">
@@ -72,59 +163,99 @@ const Navbar = () => {
             </div>
             <div
               className="max-xl:hidden cursor-pointer"
-              onClick={() => setSideBarOpen(true)}
+              onClick={openFirstSidebar}
             >
               <Menu size={40} />
             </div>
           </div>
           <img src={IIITDLogo} alt="IIITD" className="max-xl:hidden" />
         </div>
-        {sideBarOpen && (
-          <div className="fixed z-50 top-0 right-0 h-[100vh] w-full flex justify-end bg-black/20">
-            <div className="relative flex flex-col justify-center w-[30vw] bg-[#096964] text-white">
-              <button
-                onClick={() => setSideBarOpen(false)}
-                className="absolute top-[65px] right-[70px] flex items-center gap-[2px] border border-white rounded-full p-[1em]"
-              >
-                <X size={40} />
-              </button>
-              <div>
-                {navigationMap.map((linkGroup, idx) => {
-                  return (
-                    <div
-                      key={idx}
-                      onClick={() => setActiveGroup(linkGroup)}
-                      className={`flex justify-between w-full px-[70px] py-[20px] ${linkGroup.children ? "cursor-pointer" : ""}`}
+        <div
+          id="nav-bg"
+          className={`${firstSidebarOpen ? "" : "hidden"} absolute z-40 top-0 left-0 h-[100vh] w-full bg-black/20 backdrop-blur-sm`}
+          onClick={closeSidebar}
+        />
+        <div
+          id="nav-container"
+          className="fixed z-50 top-0 right-0 h-[100vh] flex justify-end"
+          style={{
+            transform: "translate(100%, 0)",
+          }}
+        >
+          <div
+            id="nav-sidebar-first"
+            className="relative flex flex-col justify-center w-[30vw] bg-[#096964] text-white"
+          >
+            <button
+              onClick={closeSidebar}
+              className="absolute top-[65px] right-[70px] flex items-center gap-[2px] border border-white rounded-full p-[1em] hover:bg-white hover:text-[#096964] transition-all ease-out duration-150"
+            >
+              <X size={40} />
+            </button>
+            <div>
+              {navigationMap.map((linkGroup, idx) => {
+                return (
+                  <div
+                    key={idx}
+                    onClick={() => {
+                      if (linkGroup.children) {
+                        setActiveGroup(linkGroup);
+                        openSecondSidebar();
+                      } else {
+                        contextSafe(() =>
+                          gsap.to("#nav-container", {
+                            x: "50%",
+                            ease: "expo.out",
+                          }),
+                        )();
+                        setActiveGroup(null);
+                      }
+                    }}
+                    className={`relative flex justify-between w-full px-[70px] py-[20px] ${linkGroup.children ? "cursor-pointer" : ""} group hover:pl-[80px] transition-all duration-[300ms]`}
+                  >
+                    <a
+                      id="nav-link-primary"
+                      href={linkGroup.url}
+                      className="relative z-10 uppercase text-[32px] leading-tight font-medium hover:underline"
                     >
-                      <a
-                        href={linkGroup.url}
-                        className="uppercase text-[32px] leading-tight font-medium hover:underline"
-                      >
-                        {linkGroup.title}
-                      </a>
-                      {linkGroup.children && (
-                        <ChevronRight size={36} color="white" />
-                      )}
-                    </div>
-                  );
-                })}
-              </div>
+                      {linkGroup.title}
+                    </a>
+                    {linkGroup.children && (
+                      <ChevronRight
+                        size={36}
+                        color="white"
+                        className={`z-10 ${activeGroup === linkGroup ? "rotate-180" : ""} transition-all duration-250`}
+                      />
+                    )}
+                    <div
+                      className={`absolute top-0 left-0 ${activeGroup === linkGroup ? "h-full" : "h-0"} w-full group-hover:h-full ${activeGroup === linkGroup ? "bg-[#033836]" : "bg-[#033836]/75"} transition-all ease-[cubic-bezier(.06,.92,.35,1)] duration-1000`}
+                    />
+                  </div>
+                );
+              })}
             </div>
-            {activeGroup && activeGroup.children && (
-              <div className="flex flex-col justify-center min-h-[50%] w-[30vw] bg-[#033836] text-white">
-                {activeGroup.children.map((child, idx) => (
+          </div>
+          <div
+            id="nav-sidebar-second"
+            className="flex flex-col justify-center min-h-[50%] w-[30vw] bg-[#033836] text-white"
+          >
+            {activeGroup &&
+              activeGroup.children &&
+              activeGroup.children.map((child, idx) => (
+                <div className="relative px-[70px] py-[20px] group">
                   <a
+                    id="nav-link-secondary"
                     key={idx}
                     href={child.url}
-                    className="px-[70px] py-[20px] uppercase text-[32px] leading-tight font-medium hover:underline"
+                    className="relative z-10 uppercase text-[32px] leading-tight font-medium hover:underline"
                   >
                     {child.title}
                   </a>
-                ))}
-              </div>
-            )}
+                  <div className="absolute top-0 left-0 h-0 w-full group-hover:h-full bg-[#096964]/75 transition-all ease-[cubic-bezier(.06,.92,.35,1)] duration-1000" />
+                </div>
+              ))}
           </div>
-        )}
+        </div>
       </nav>
     </header>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,32 @@
 import Navbar from "../../components/Navbar";
 import background from "../../assets/testing.svg";
+import PageReveal from "../../components/PageReveal";
+import { useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+gsap.registerPlugin(useGSAP);
 
 function App() {
+  const [revealAnimationComplete, setRevealAnimationComplete] = useState(false);
+
+  useGSAP(() => {
+    const timeline = gsap.timeline({
+      defaults: { ease: "power4.inOut", duration: 2 },
+    });
+
+    if (!revealAnimationComplete) {
+      timeline.to("#page-reveal-container", {
+        y: "-100%",
+        delay: 3,
+        onComplete: () => setRevealAnimationComplete(true),
+      });
+    }
+  });
+
   return (
     <>
+      {revealAnimationComplete ? "" : <PageReveal />}
       <div className="min-h-screen bg-white font-sans">
         <Navbar />
         <main className="relative w-full min-h-[calc(100vh-176px)] mt-[176px]">
